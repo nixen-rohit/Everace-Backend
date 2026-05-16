2db833bd1ba2c9e7b4a281328436e032cd83ec63
akashnegi62 <akashsing215@gmail.com>
2026-05-16 15:38:54 +0530

    server ready

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:3000",
-  process.env.CLIENT_URL,
+  process.env.CLIENT_URL || "https://everacee-seven.vercel.app",
 ].filter(Boolean);
 
 app.use(
